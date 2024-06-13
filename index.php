9df49c25aa2a20e36657294934c62e0796a2ae12
glerique <gael.lerique@gmail.com>
2024-06-13 15:17:37 +0200

    Refactoring index file

--- index.php
+++ index.php
@@ -2,10 +2,25 @@
 require_once 'vendor/autoload.php';
 require_once 'config/config.php';
 
-use App\Lib\Application;
-/** Pour afficher les erreur en Dev */
 ini_set('display_errors', 1);
 
-Application::start();
+use App\Lib\Application;
+use App\Lib\QueryBuilder;
+use App\Lib\DatabaseConfig;
+use App\Lib\DatabaseConnection;
+use App\Factory\ControllerFactory;
+
+// Configurer les dépendances
+$config = new DatabaseConfig(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD);
+$dbConnection = new DatabaseConnection($config);
+$queryBuilder = new QueryBuilder($dbConnection);
+
+// Créer les objets nécessaires
+$controllerFactory = new ControllerFactory($queryBuilder);
 
 
+// Initialiser l'application
+$app = new Application($controllerFactory);
+
+// Démarrer l'application
+$app->start();
