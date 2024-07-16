910be9a733419d4f75815c14467b9052d6c751d9
glerique <gael.lerique@gmail.com>
2024-07-16 12:04:52 +0200

    [#9] Declaration of controllers as Symfony services and removed ControllerFactory

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;	
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 $container = new ContainerBuilder();
 $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
