0a0043146f7958bc5d53e1735e22c5437b379236
glerique <gael.lerique@gmail.com>
2024-07-23 15:53:37 +0200

    [#10] Add the Symfony DotEnv component

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-const DB_HOST = "localhost";
-const DB_USER = "admin";
-const DB_PASSWORD = "test";
-const DB_NAME = "employees";
-const SMTP_USERNAME = "";
-const SMTP_PASSWORD = "";
--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
+error_reporting(E_ALL & ~E_DEPRECATED);
+
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
+
 $container = new ContainerBuilder();
 $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
 $loader->load('services.yaml');
-$container->compile();
+
+$container->compile(true);
+
 $app = $container->get(App\Lib\Application::class);
 $app->start();
+
